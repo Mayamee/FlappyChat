f3349a03a8e0617f250f2fcede4c5f0b6032ff4f
Mayamee <ponyashcat228@gmail.com>
2023-03-26 18:22:49 +0300

    navbar link done

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
+import { Link } from 'react-router-dom'
 
 const Layout = ({ children }) => {
   return (
     <div className="d-flex flex-column min-vh-100">
       <header>
-        <Navbar className="shadow-sm" bg="white">
+        <Navbar as={Link} to="/" className="shadow-sm text-decoration-none" bg="white">
           <Container>
             <Navbar.Brand>Hexlet Chat</Navbar.Brand>
           </Container>
