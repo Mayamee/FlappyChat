6489f2a97a563d915668272640c04476b91b1d88
Mayamee <ponyashcat228@gmail.com>
2023-04-02 11:25:19 +0300

    focus on login when authentication fails

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -4,8 +4,9 @@ import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import loginSchema from '@validation/loginSchema'
 import AuthService from '@services/AuthService'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { FloatingLabel } from 'react-bootstrap'
+import clsx from 'clsx'
 // import Debug from '@utils/Debug/Debug'
 
 const initialValues = {
@@ -15,14 +16,19 @@ const initialValues = {
 
 const LoginForm = () => {
   const [authFailed, setAuthFailed] = useState(false)
+  const loginRef = useRef(null)
   const onSubmitHandler = async (values, actions) => {
     try {
       const data = await AuthService.login(values.login, values.password)
       console.log(data)
     } catch (err) {
       if (err.isAxiosError && err.response.status === 401) {
+        const { current: loginInput } = loginRef
+        if (loginInput) {
+          loginInput.focus()
+          loginInput.select()
+        }
         setAuthFailed(true)
-        // TODO Foucus on first field
         return
       }
       throw err
@@ -31,64 +37,55 @@ const LoginForm = () => {
     }
   }
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmitHandler}
-      validationSchema={loginSchema}
-      initialErrors={{
-        login: '',
-        password: '',
-      }}
-    >
+    <Formik initialValues={initialValues} onSubmit={onSubmitHandler} validationSchema={loginSchema}>
       {({ values, handleBlur, handleChange, isSubmitting }) => (
         <FormikForm>
           <h1 className="text-center mb-3">Войти</h1>
-          <fieldset disabled={isSubmitting}>
-            <Form.Group className="mb-3">
-              <FloatingLabel controlId="login" label="Ваш ник">
-                <Form.Control
-                  value={values.login}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  isInvalid={authFailed}
-                  name="login"
-                  autoComplete="off"
-                  type="text"
-                  placeholder="Ваш ник"
-                  required
-                />
-              </FloatingLabel>
-            </Form.Group>
-            <Form.Group className="mb-3 position-relative">
-              <FloatingLabel controlId="password" label="Введите пароль">
-                <Form.Control
-                  value={values.password}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  name="password"
-                  isInvalid={authFailed}
-                  autoComplete="off"
-                  type="password"
-                  placeholder="Введите пароль"
-                  required
-                />
-                <Form.Control.Feedback type="invalid" tooltip>
-                  Неверный логин или пароль :(
-                </Form.Control.Feedback>
-              </FloatingLabel>
-            </Form.Group>
-            <Button
-              className="w-100"
-              type="submit"
-              variant="outline-primary"
-              disabled={isSubmitting}
-            >
-              {isSubmitting && (
-                <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
-              )}
-              {!isSubmitting && 'Войти'}
-            </Button>
-          </fieldset>
+          <Form.Group className="mb-3">
+            <FloatingLabel controlId="login" label="Ваш ник">
+              <Form.Control
+                ref={loginRef}
+                value={values.login}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                isInvalid={authFailed}
+                name="login"
+                autoComplete="off"
+                type="text"
+                placeholder="Ваш ник"
+                required
+              />
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group
+            className={clsx('position-relative', {
+              'mb-3': !authFailed,
+              'mb-5': authFailed,
+            })}
+          >
+            <FloatingLabel controlId="password" label="Введите пароль">
+              <Form.Control
+                value={values.password}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                name="password"
+                isInvalid={authFailed}
+                autoComplete="off"
+                type="password"
+                placeholder="Введите пароль"
+                required
+              />
+              <Form.Control.Feedback type="invalid" tooltip>
+                Неверный логин или пароль
+              </Form.Control.Feedback>
+            </FloatingLabel>
+          </Form.Group>
+          <Button className="w-100" type="submit" variant="outline-primary" disabled={isSubmitting}>
+            {isSubmitting && (
+              <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
+            )}
+            {!isSubmitting && 'Войти'}
+          </Button>
           {/* <Debug /> */}
         </FormikForm>
       )}
