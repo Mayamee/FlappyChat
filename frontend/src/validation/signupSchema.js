afe3bd2d3a6b24649a5f70f3f86e0b68f2f86cc1
Mayamee <ponyashcat228@gmail.com>
2023-04-23 20:08:09 +0300

    fix signup schema

--- frontend/src/validation/signupSchema.js
+++ frontend/src/validation/signupSchema.js
@@ -5,10 +5,11 @@ export default (text) =>
     login: yup
       .string()
       .min(3, text.login?.min || 'Длина логина должна быть минимум 3 знака')
+      .max(20, text.login?.max || 'Длина логина должна быть максимум 20 знаков')
       .required(text.login?.required || 'Обязательное поле'),
     password: yup
       .string()
-      .min(5, text.password?.min || 'Длина пароля должна быть минимум 5 знаков')
+      .min(6, text.password?.min || 'Длина пароля должна быть минимум 6 знаков')
       .required(text.password?.required || 'Обязательное поле'),
     confirmPassword: yup
       .string()
