6bd0a5f7deb5050babe3aafaa456d267bd3c1b4b
Mayamee <ponyashcat228@gmail.com>
2023-04-23 20:19:19 +0300

    add production enviroment

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,13 +1,14 @@
 import { Container } from 'react-bootstrap'
 import ChatBox from '@/components/ChatBox/ChatBox'
 import { SocketProvider } from '@/hooks/useSocket'
+import { SOCKET_URL } from '@/vars/socket'
 
 const Chat = () => {
   return (
     <Container className="flex-fill py-4">
       <SocketProvider
         opts={{
-          url: 'http://localhost:8080',
+          url: SOCKET_URL,
         }}
       >
         <ChatBox />
---
+++ frontend/src/vars/socket.js
@@ -0,0 +1,1 @@
+export const { REACT_APP_SOCKET_URL: SOCKET_URL } = process.env || 'http://localhost:8080'
